1f12a1a1e14ff1bdb91e38ff9d8a04e8dff818ae
Vitalii <sharaiev.pn@ucu.edu.ua>
2022-10-30 12:39:38 +0200

    Created language_classes() function. Created get_words() function.

--- target_ua.py
+++ target_ua.py
@@ -1,7 +1,8 @@
 from typing import List
 import random
 
-def generate_grid() -> List[List[str]]:
+
+def generate_grid() -> List[str]:
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. ["а", "і", "с", "х", "д"]
@@ -13,13 +14,42 @@ def generate_grid() -> List[List[str]]:
         if char not in board:
             board.append(char)
 
+    return board
 
-def get_words(f: str, letters: List[str]) -> List[str]:
+
+def language_classes() -> str:
+    """
+    Generates certain language class
+    """
+    part = random.choice(['Дієслово', "Іменник", "Прислівник", "Прикметник"])
+    return part
+
+
+def get_words(f: str, letters: List[str]) -> List[tuple]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    pass
+    possible_words = []
+    with open(f, 'r') as file:
+        file = file.read().split('\n')
+        for row in file:
+            if len(row) < 1 or len(row.split()[0]) > 5 or\
+               row.split()[0][0] not in ''.join(letters) or\
+               'intj' in row or 'noninfl' in row:
+                continue
+            if 'noun' in row or '/n' in row:
+                possible_words.append((row.split()[0], "Іменник"))
 
+            if 'verb' in row or '/v' in row:
+                possible_words.append((row.split()[0], 'Дієслово'))
+
+            if len(row.split()) == 2 and row.split()[1] == 'adv':
+                possible_words.append((row.split()[0], "Прислівник"))
+
+            if len(row.split()) == 2 and row.split()[1] == '/adj':
+                possible_words.append((row.split()[0], "Прикметник"))
+
+    return possible_words
 
 
 def get_user_words() -> List[str]:
